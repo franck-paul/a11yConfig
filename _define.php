8fda05ed8a3e8c4bcd7c798a18555fa13fbb70ea
franck <carnet.franck.paul@gmail.com>
2019-10-31 11:19:06 +0100

    Le squelette du plugin a11yConfig

---
+++ _define.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @brief a11yConfig, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul, Biou and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('DC_RC_PATH')) {return;}
+
+$this->registerModule(
+    "a11yConfig",                                           // Name
+    "Implements Access42 accessibility configuration tool", // Description
+    "Franck Paul, Biou and contributors",                   // Author
+    '1.0',                                                  // Version
+    [
+        'requires'    => [['core', '2.15']],
+        'permissions' => 'admin',                                     // Permissions
+        'type'        => 'plugin',                                    // Type
+        'support'     => 'https://github.com/franck-paul/a11yConfig', // Support URL
+        'settings'    => ['pref' => '#user-options.a11yConfig']      // Settings
+    ]
+);
