d54eb60597894f5d080a2f2b50f8e2f07bbafba6
franck <carnet.franck.paul@gmail.com>
2020-01-03 17:01:29 +0100

    Use classList.add(…) rather than className += …

--- js/public.js
+++ js/public.js
@@ -4,7 +4,7 @@ function a11yconfig_load() {
   const images = document.getElementsByTagName('img');
   for (let i = 0; i < images.length; i++) {
     if (images[i].alt !== '') {
-      images[i].className += ' a42-ac-replace-img';
+      images[i].classList.add('a42-ac-replace-img');
     }
   }
 }
