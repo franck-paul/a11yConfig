b982c75fb75d6210ee36846ad7ccecb7373b9de4
franck <carnet.franck.paul@gmail.com>
2020-02-06 15:14:47 +0100

    Typo! should search for alt="" not tag="" ;-)

--- js/admin.js
+++ js/admin.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener('load', () => {
-  const images = document.querySelectorAll('img:not([tag=""])');
+  const images = document.querySelectorAll('img:not([alt=""])');
   images.forEach(function(image) {
     image.classList.add('a42-ac-replace-img');
   });
--- js/public.js
+++ js/public.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('load', () => {
-  const images = document.querySelectorAll('img:not([tag=""])');
+  const images = document.querySelectorAll('img:not([alt=""])');
   images.forEach(function(image) {
     image.classList.add('a42-ac-replace-img');
   });
