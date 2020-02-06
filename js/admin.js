7201cf4e6575e80cddedacf3f90df8cbdbaf029a
franck <carnet.franck.paul@gmail.com>
2020-02-06 11:39:22 +0100

    JS refactoring

--- js/admin.js
+++ js/admin.js
@@ -15,10 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener('load', () => {
-  const images = document.getElementsByTagName('img');
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].alt !== '') {
-      images[i].classList.add('a42-ac-replace-img');
-    }
-  }
+  const images = document.querySelectorAll('img:not([tag=""])');
+  images.forEach(function(image) {
+    image.classList.add('a42-ac-replace-img');
+  });
 });
