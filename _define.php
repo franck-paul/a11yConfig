d17e51513a25f46d2292580877890b25bcba713f
franck <carnet.franck.paul@gmail.com>
2025-02-04 11:55:41 +0100

    Add release date information in _define.php, dcstore.xml and cope with it in github action

--- _define.php
+++ _define.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief a11yConfig, a plugin for Dotclear 2
  *
@@ -16,6 +17,7 @@ $this->registerModule(
     'Franck Paul, Biou and contributors',
     '5.4',
     [
+        'date'        => '2003-08-13T13:42:00+0100',
         'requires'    => [['core', '2.29']],
         'permissions' => 'My',
         'type'        => 'plugin',
